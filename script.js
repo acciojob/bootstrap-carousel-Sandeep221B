b7e07a3930ec74ddfa773a30df73c066fcf3ec53
Sandeep221B <molletisandeep10@gmail.com>
2024-11-29 15:17:13 +0530

    created a submission

--- script.js
+++
@@ -1,9 +0,0 @@
-describe('Carousel tests', () => {
-  it('Should render next or previous icon', () => {
-    // Check for previous icon
-    cy.get('.carousel-control-prev-icon').should('exist');
-
-    // Check for next icon
-    cy.get('.carousel-control-next-icon').should('exist');
-  });
-});
